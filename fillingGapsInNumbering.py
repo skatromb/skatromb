fd7653f02d84b191cc9ef8255512a0bf56744c07
skatromb <skatromb@yandex.ru>
2019-11-05 08:16:17 +0300

    Сделал typing

--- fillingGapsInNumbering.py
+++ fillingGapsInNumbering.py
@@ -2,31 +2,41 @@
 # with a given prefix (spam001.txt, span002.txt for example)
 # and renames it to the numbering with no gaps
 
-import os, re, typing
-listStrType = typing.List[str]
-DictStrType = typing.
-listDictType = typing.List[dict[]]
+import os, re
+from typing import TypedDict, List
 
 
-# TODO: find all the files with a given prefix and return list of this files
-def find_files_with_prefix(prefix:str, folder='.') -> listStrType:
-    regexp = re.compile(r'(^' + prefix + r')(\d+)(\..{,4}$)')
-    print(regexp.search('spam001.txt').groups())
+class ParsedFilename(TypedDict):
+    filename: str
+    prefix: str
+    numbering: int
+    postfix: str
+
+
+# TODO: find all the files with a given prefix and return list of [(filename + prefix + numbering + postfix)]
+def find_and_parse_files_with_prefix(prefix: str, folder='.') -> List[ParsedFilename]:
+    # TODO: обходим папку в поисках файлов с префиксом и нумерацией
+    #   Формируем лист файлов внутри которого будут наименования и части наименований (лист диктов)
+    for root, dirnames, filenames in os.walk(folder):
+        for filename in filenames:
+            continue
+
+    # files_list = [re.search(r'((^' + prefix + r')(\d+)(\..{,4}$))', filename).groups()]
+
+    # print(regexp.findall('spam001.txt').groups())
     return list
 
 
-# TODO: parse the list of files and return a fullname + prefix + numbering + postfix
-def filename_parser(filenames:list[str]) -> listDictType: # dict: {'fullname', 'prefix', 'numbering', 'postfix'}
-    return
+class OldNewFilename(TypedDict):
+    new_filename_for_old: str
 
 
 # TODO: order the files list by numbering and reassign a right numbering to the files
 #   Return the old-new naming of the files
-def files_renamer(parsed_filenames:list[dict]) -> listDictType: # same type as filename_parser
+def files_renamer(parsed_filenames: List[ParsedFilename]) -> List[OldNewFilename]:  # TODO: add typing. Ret
     return
 
 
-
-files_with_prefix = find_files_with_prefix(prefix='spam', folder='.')
-parsed_filenames = filename_parser(files_with_prefix)
-print(files_renamer(parsed_filenames))
+#   TODO: Main program
+files_with_prefix = find_and_parse_files_with_prefix(prefix='spam', folder='.')
+# print(files_renamer(files_with_prefix))
