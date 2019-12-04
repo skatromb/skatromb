648a2efc9a36fbb490f2e3b5f152161047bc0c99
skatromb <ivan.livadnyy@tele2.ru>
2019-12-04 14:05:44 +0300

    Добавил Delete

--- code/Tele2ForSQL/HistoricalDataScriptNEs.py
+++ code/Tele2ForSQL/HistoricalDataScriptNEs.py
@@ -5,15 +5,17 @@ from calendar import Calendar
 ENV = 'DEV'  # 'PRD2'
 if ENV == 'DEV':
     Layer = 'DDS'
-    Postfix = '_LIVADNYY'
+    Postfix_prc = '_LIVADNYY'
+    Postfix_tbl = '_2'
 else:
     Layer = 'MDS'
-    Postfix = ''
+    Postfix_prc = ''
+    Postfix_tbl = ''
 
 
 def generate_sql_for_month(year: date.year, month: date.month):
 
-    def call_procedure_without_params(procedure_name: str, env: str = ENV, layer: str = Layer, postfix: str = Postfix) -> str:
+    def call_procedure_without_params(procedure_name: str, env: str = ENV, layer: str = Layer, postfix: str = Postfix_prc) -> str:
         call_statement = 'CALL ' + env + '_' + layer + '.' + procedure_name + postfix
         return call_statement
 
@@ -27,13 +29,23 @@ def generate_sql_for_month(year: date.year, month: date.month):
 
     # Блок месячных расчётов
     print()
-    params = "(7273889, date'" + date(year, month, 1).isoformat() + "');"
+    params = "(date'" + date(year, month, 1).isoformat() + "', 7273889);"
 
     print(call_procedure_without_params('LOAD_NETWORK_ELEMENT_MONTHLY_METRICS') + params)
     print()
     print(call_procedure_without_params('LOAD_SITE_MONTHLY_METRICS') + params)
 
-    print('\n' + '-' * 120)
+    # Удаляем за рассчитанный месяц информацию
+    print()
+    date_str = "'" + date(year, month, 1).isoformat() + "'"
+    print("DELETE " + ENV + "_DDS.NE_SUBS_REVENUE_DATE" + Postfix_tbl + " WHERE REPORT_DATE BETWEEN "
+          "date" + date_str + " AND ADD_MONTHS(date" + date_str + ", 1) - 1;")
 
 
-generate_sql_for_month(year=2019, month=9)
+# 2019 с октября
+for Month in range(10, 0, -1):
+    generate_sql_for_month(year=2019, month=Month)
+
+# 2018 до февраля
+for Month in range(12, 1, -1):
+    generate_sql_for_month(year=2018, month=Month)
