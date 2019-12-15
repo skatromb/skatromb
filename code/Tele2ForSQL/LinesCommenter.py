369371f3d3e36abd66d396eec9c95b859de8b8ed
skatromb <skatromb@yandex.ru>
2019-12-15 23:14:41 +0300

    Нахуярил

--- code/Tele2ForSQL/LinesCommenter.py
+++ code/Tele2ForSQL/LinesCommenter.py
@@ -1,12 +1,25 @@
 import os
 import re
+from typing import List, Tuple
+
+
+ENCODING = 'UTF-8'
 
 
 # генератор files_dict = {str(i) + '.sql': ['1\n', '2\n, 3'] * (i + 1) for i in range(2)}
 # Нужно закомментировать в sql-файлах substrs_to_comment
-def comment_lines(filenames: list[str], substrs_to_comment: tuple[str]):
-    def get_files_w_line_numbers_to_comment() -> dict[str: [tuple[int]]]: # {filename: [line_number]}
-        def get_line_numbers(filename: str) -> tuple[int]:
+def check_and_comment(filenames: List[str], substrs_to_comment: Tuple[str]):
+
+    def files_existence_check():
+        # Проверяем список путей на валидность
+        for filename in filenames:
+            if not os.path.exists(filename):
+                raise Exception('Указанного пути к файлу не существует')
+        return True
+    files_existence_check()
+
+    def get_files_w_line_numbers_to_comment() -> dict: # {filename: [line_number]}
+        def get_line_numbers(filename: str) -> Tuple[int]:
             def line_contains_substrs(line: str) -> bool:
                 # line содержит любую из substrs_to_comment?
                 # TODO: Попробовать переписать в map()
@@ -14,7 +27,7 @@ def comment_lines(filenames: list[str], substrs_to_comment: tuple[str]):
                                 for substr in substrs_to_comment]
 
             # Вернём line_number'ы из файлов с вхождениями substrs_to_comment
-            code_lines = open(filename, encoding='UTF-8').readlines()
+            code_lines = open(filename, encoding=ENCODING).readlines()
             return tuple([line_number for line_number, code_line in enumerate(code_lines)
                             if line_contains_substrs(code_line)])
 
@@ -22,29 +35,35 @@ def comment_lines(filenames: list[str], substrs_to_comment: tuple[str]):
         # Пример формата {'1.sql', ['--\n', 'CREATE TABLE...;', ..., 'END;']
         return {filename: get_line_numbers(filename)
                 for filename in filenames}
+    files_w_line_numbers_to_comment = get_files_w_line_numbers_to_comment()
 
-    files_w_line_numbers_to_comment = get_files_w_line_numbers_to_comment(filenames, substrs_to_comment)
+    def show_lines_to_comment():
+        # Показываем пользователю, какие строки в каких файлах мы собираемся закомментить
+        for filename, line_numbers in files_w_line_numbers_to_comment.items():
+            file = open(filename, mode='w', encoding=ENCODING)
+            code_lines = file.readlines()
+            print('\n' + filename + ':\n')
 
-    # Проверяем список путей на валидность
-    for filename in files_w_line_numbers_to_comment:
-        if not os.path.exists(filename):
-            raise Exception('Указанного пути к файлу не существует')
+            for i in line_numbers:
+                print(code_lines[i])
+            return
 
-    # Теперь нужно открыть все найденные файлы и закомментить им строки
-    for filename, line_numbers in files_w_line_numbers_to_comment.items():
-        file = open(filename, mode='w', encoding='UTF-8')
-        code_lines = file.readlines()
-        print(filename + ':\n')
-
-        # Нужно показать все файлы в принте пользователю
-            # Нужно показать все строки в принте пользователю
-
-        # TODO: Если пользователь согласен, комментим строки
-        if input('Закомментить эти строки? (y/n)').lower() in ('y', 'yes'):
-
-            file.writelines(list(map(lambda line: '-- ' + line, code_lines)))
+    def comment_lines():
+        show_lines_to_comment()
+        if input('Комментим эти строки? (y/yes, n/no)').lower() in ('y', 'yes'):
+            for filename, line_numbers in files_w_line_numbers_to_comment.items():
+                file = open(filename, mode='w', encoding=ENCODING)
+                code_lines = file.readlines()
+                for line_number in line_numbers:
+                    code_lines[line_number] = '-- ' + code_lines[line_number]
+                    # list(map(lambda line: '-- ' + line, code_lines)))
+        else:
+            print('Ладно, не буду комментить строки')
         file.close()
+        return
 
+
+    comment_lines()
 
     return None
 
@@ -112,4 +131,4 @@ SUBSTRS_TO_COMMENT = [
     'CNT_UNIQUE_SERVICE_DISCOUNT'
 ]
 
-comment_lines(FILE_NAMES, SUBSTRS_TO_COMMENT)
+check_and_comment(FILE_NAMES, SUBSTRS_TO_COMMENT)
