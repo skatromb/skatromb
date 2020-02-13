2714b057e8582f6fb4627d24c1869d3f3ee72001
skatromb <ivan.livadnyy@tele2.ru>
2020-02-13 10:24:36 +0300

    asyncio_use.py (не работает)

--- src/skatromb/other/concurrency/asyncio_use.py
+++ src/skatromb/other/concurrency/asyncio_use.py
@@ -1,6 +1,9 @@
+# FIXME: Не работает, виснет на строке print()
 import asyncio
 import time
 import aiohttp
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 
 async def download_site(session, url):
@@ -8,10 +11,10 @@ async def download_site(session, url):
         print("Read {0} from {1}".format(response.content_length, url))
 
 
-async def download_all_sites(sites):
+async def download_all_sites(urls):
     async with aiohttp.ClientSession() as session:
         tasks = []
-        for url in sites:
+        for url in urls:
             task = asyncio.ensure_future(download_site(session, url))
             tasks.append(task)
         await asyncio.gather(*tasks, return_exceptions=True)
@@ -21,8 +24,8 @@ if __name__ == "__main__":
     sites = [
         "https://www.jython.org",
         "http://olympus.realpython.org/dice",
-    ] * 80
+    ] * 100
     start_time = time.time()
-    asyncio.get_event_loop().run_until_complete(download_all_sites(sites))
+    asyncio.run(download_all_sites(sites))
     duration = time.time() - start_time
     print(f"Downloaded {len(sites)} sites in {duration} seconds")
