21ac2ae7ef1592ab27614b8872cc87dcd4ae772a
skatromb <ivan.livadnyy@tele2.ru>
2020-02-14 08:16:15 +0300

    CPU-bound

---
+++ src/skatromb/other/concurrency/cpu_bound/synchronous.py
@@ -0,0 +1,26 @@
+import time
+import logging
+
+
+def cpu_bound(number):
+    return sum(i * i for i in range(number))
+
+
+def find_sums(numbers):
+    for number in numbers:
+        logger.info(str(number))
+        cpu_bound(number)
+
+
+logging.basicConfig(format='%(asctime)s (UTC): %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logging.basicConfig()
+
+if __name__ == "__main__":
+    numbers = [5_000_000 + x for x in range(20)]
+
+    start_time = time.time()
+    find_sums(numbers)
+    duration = time.time() - start_time
+    print(f"Duration {duration} seconds")
