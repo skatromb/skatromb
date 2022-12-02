19182e5e7d04d6371662e86d27ce8693220cf7fa
skatromb <1759463+skatromb@users.noreply.github.com>
2022-12-02 16:11:16 +0100

    Deleted ansible stuff

--- ansible-playbook/ansible-apply.sh
+++
@@ -1,5 +0,0 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" && \
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-brew install ansible &&
-ansible-galaxy install -r requirements.yml
-ansible-playbook playbook.yml
