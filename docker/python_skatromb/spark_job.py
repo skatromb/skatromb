8b35b1d9277b92c9079011c06b69ae66ef597045
skatromb <1759463+skatromb@users.noreply.github.com>
2022-12-05 18:09:26 +0100

    spark job

---
+++ docker/python_skatromb/spark_job.py
@@ -0,0 +1,38 @@
+# from pyspark.sql.session import SparkSession as spark
+
+from os import environ
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+
+tz = ZoneInfo("CET")
+print(started_at := datetime.now(tz))
+
+# Profiles = 15m rows
+df = (spark
+      .read
+      .format("jdbc")
+      .option("url", environ["DB_URL"])
+      .option("user", environ["DB_USER"])
+      .option("password", environ["DB_PASSWORD"])
+      .option("dbtable", f"{environ['DB_TABLE']}")
+      # .option("fetchsize", f"{10_000}")
+      # .option("queryTimeout", 4*60*60)
+      # partitioning try
+      # .option("partitionColumn", "ID")
+      # .option("lowerBound", "0")
+      # .option("upperBound", "20000000")
+      # .option("numPartitions", 128)
+
+      .load()
+      )
+
+(df
+ .write
+ .parquet("/Users/skatromb/code/skatromb/jdbc_test",
+          mode="overwrite")
+ )
+
+print(ended_at := datetime.now(tz))
+
+print(f"TIME PASSED: {ended_at - started_at}")
