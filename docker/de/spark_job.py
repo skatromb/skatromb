8d1695ea983bf6962b9f6cdbebcb38eb751aa8fb
skatromb <1759463+skatromb@users.noreply.github.com>
2022-12-09 11:19:23 +0100

    Proper parallelism

--- docker/de/spark_job.py
+++ docker/de/spark_job.py
@@ -1,8 +1,8 @@
-# from pyspark.sql.session import SparkSession as spark
-
 from datetime import datetime
 from os import environ
 from zoneinfo import ZoneInfo
+
+from pyspark.sql.session import SparkSession
 
 tz = ZoneInfo("CET")
 print(started_at := datetime.now(tz))
@@ -10,8 +10,31 @@ print(started_at := datetime.now(tz))
 db_url = environ["DB_URL"]
 db_schema = environ["DB_SCHEMA"]
 db_table = environ["DB_TABLE"]
+db_indexed_column = environ["DB_INDEXED_COLUMN"]
 db_user = environ["DB_USER"]
 db_password = environ["DB_PASSWORD"]
+
+spark = SparkSession.builder.getOrCreate()
+
+min_indexed_col, max_indexed_col = (
+    spark.read.format("jdbc")
+    .option("url", f"jdbc:mysql://{db_url}/{db_schema}")
+    .option("user", db_user)
+    .option("password", db_password)
+    .option(
+        "query",
+        "\n".join(
+            [
+                "SELECT",
+                f"    MIN({db_indexed_column}) AS lower_bound,",
+                f"    MAX({db_indexed_column}) AS upper_bound",
+                f"FROM {db_table}",
+            ]
+        ),
+    )
+    .load()
+    .collect()[0]
+)
 
 # Profiles = 15m rows
 df = (
@@ -21,15 +44,15 @@ df = (
     .option("password", db_password)
     .option("dbtable", f"{db_schema}.{db_table}")
     # partitioning try
-    # .option("partitionColumn", "ID")
-    # .option("lowerBound", "0")
-    # .option("upperBound", "20000000")
-    # .option("numPartitions", 128)
+    .option("partitionColumn", db_indexed_column)
+    .option("lowerBound", min_indexed_col)
+    .option("upperBound", max_indexed_col)
+    .option("numPartitions", 100)
     .load()
 )
 
 (
-    df.write.partitionBy().parquet(
+    df.write.parquet(
         f"/Users/skatromb/code/skatromb/outputs/spark/to_parquet_{db_table}",
         mode="overwrite",
     )
