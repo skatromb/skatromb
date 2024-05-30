bc9b6435eb907e847fd652d6a4f4401ec9315bd5
skatromb <1759463+skatromb@users.noreply.github.com>
2024-05-30 20:27:15 +0200

    multiple refs

--- src/rust/04_ownership/mut_ref/src/main.rs
+++ src/rust/04_ownership/mut_ref/src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
     let mut s = String::from("hello");
-    
-    change(&mut s);
-    
-    println!("{}", s);
-}
-
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
