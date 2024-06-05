5c7eca13e82b9ca051ccf61ec9df3a619e083c4a
skatromb <1759463+skatromb@users.noreply.github.com>
2024-06-05 15:11:19 +0200

    sandbox

---
+++ src/rust/05_structs/sandbox/src/main.rs
@@ -0,0 +1,15 @@
+struct User {
+    active: bool,
+    username: &str,
+    email: &str,
+    sign_in_count: u64,
+}
+
+fn main() {
+    let user1 = User {
+        active: true,
+        username: "someusername123",
+        email: "someone@example.com",
+        sign_in_count: 1,
+    };
+}
