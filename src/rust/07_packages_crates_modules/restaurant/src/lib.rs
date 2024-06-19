51aaa9d9a434d3e70ae7ec4e925c08cdb7887787
skatromb <1759463+skatromb@users.noreply.github.com>
2024-06-19 08:25:06 +0200

    optimise imports

--- src/rust/07_packages_crates_modules/restaurant/src/lib.rs
+++ src/rust/07_packages_crates_modules/restaurant/src/lib.rs
@@ -3,14 +3,14 @@ mod blah;
 mod front_of_house {
     pub mod hosting {
         pub(crate) fn add_to_waitlist() {}
-        
+
         fn seat_at_table() {}
     }
     mod serving {
         fn take_order() {}
-        
+
         fn serve_order() {}
-        
+
         fn take_payment() {}
     }
 }
@@ -22,12 +22,12 @@ mod back_of_house {
         Soup,
         Salad,
     }
-    
+
     pub struct Breakfast {
         pub toast: String,
         seasonal_fruit: String,
     }
-    
+
     impl Breakfast {
         pub fn summer(toast: &str) -> Breakfast {
             Breakfast {
@@ -40,21 +40,19 @@ mod back_of_house {
         cook_order();
         super::deliver_order();
     }
-    
+
     fn cook_order() {}
 }
 
 
 
 mod customer {
-    use crate::front_of_house;
-    use crate::front_of_house::hosting::add_to_waitlist;
-    use crate::back_of_house::Appetizer;
-    use crate::back_of_house::Breakfast;
+    use crate::front_of_house::{self, hosting};
+    pub use crate::back_of_house::{Appetizer, Breakfast};
     
     pub fn eat_at_restaurant() {
         front_of_house::hosting::add_to_waitlist();
-        add_to_waitlist();
+        hosting::add_to_waitlist();
 
         let order1 = Appetizer::Soup;
         let order2 = Appetizer::Salad;
