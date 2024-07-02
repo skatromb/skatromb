86d17667bb0b5077ed9107202a7de25949a1bbd4
skatromb <1759463+skatromb@users.noreply.github.com>
2024-07-02 22:57:22 +0200

    Second version of `pig_latin`

--- src/rust/08_collections/pig_latin/src/main.rs
+++ src/rust/08_collections/pig_latin/src/main.rs
@@ -11,17 +11,17 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
 
-    let mut output = String::new();
+    let mut output: Vec<String> = Vec::new();
     for word in input.split_whitespace() {
         let mut chars = word.chars();
         let (first, rest) = (chars.next().unwrap(), chars.as_str());
-        if CONSONANTS.contains(&first) {
-            let appendix = format!("{}-{}ay ", rest, first);
-            output.push_str(appendix.as_str());
+        if CONSONANTS.contains(&first) && !rest.is_empty() {
+            let appendix = format!("{}-{}ay", rest, first);
+            output.push(appendix);
         }
         else {
-            output.push_str(format!("{}-hay ", word).as_str());
+            output.push(format!("{}-hay", word));
         }
     }
-    println!("{}", output);
+    println!("{}", output.join(" "));
 }
