614a34cd9c2d7f8a2d4bfeaaf1749390868f5a45
skatromb <1759463+skatromb@users.noreply.github.com>
2024-07-18 16:07:30 +0200

    sandbox

--- src/rust/10_generics/sandbox/src/main.rs
+++ src/rust/10_generics/sandbox/src/main.rs
@@ -1,15 +1,24 @@
 fn main() {
-    let string1 = "abcd".to_string();
-    let string2 = "xyz";
-    
-    let result = longest::<&str>(string1.as_str(), string2);
-    println!("The longest string is {result}");
+    // let string1 = String::from("long string is long");
+    // let result: &str;
+    // {
+    //     let string2 = String::from("xyz");
+    //     result = longest(string1.as_str(), string2.as_str());
+    // }
+    // println!("The longest string is {result}");
+    let string2 = create_str();
+    println!("{string2}");
 }
 
-fn longest<'a, T: PartialOrd>(x: &'a str, y: &'a str) -> &'a str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
         y
     }
+}
+
+
+fn create_str() -> String {
+    String::from("really long string")
 }
