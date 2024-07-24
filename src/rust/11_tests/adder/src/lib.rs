ab94b83660fb142158d75bdae7536fbcc266f7d5
skatromb <1759463+skatromb@users.noreply.github.com>
2024-07-24 12:31:15 +0200

    tests

---
+++ src/rust/11_tests/adder/src/lib.rs
@@ -0,0 +1,28 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+    
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2))
+    }
+    
+    #[test]
+    fn failing() {
+        panic!("Make this test fail");
+    }
+}
