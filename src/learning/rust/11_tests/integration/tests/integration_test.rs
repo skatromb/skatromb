4c330be6fbb86235fafb7317f3d22a24c6fd8356
skatromb <1759463+skatromb@users.noreply.github.com>
2024-08-08 10:55:04 +0200

    tests

---
+++ src/learning/rust/11_tests/integration/tests/integration_test.rs
@@ -0,0 +1,8 @@
+use integration::add_two;
+mod common;
+
+#[test]
+fn it_adds_two() {
+    common::setup();
+    assert_eq!(4, add_two(2));
+}
--- src/learning/rust/json/src/lib.rs
+++ src/learning/rust/json/src/lib.rs
@@ -1,19 +1,19 @@
 use std::collections::HashMap;
 use std::ops::Index;
 
-#[derive(Clone)]
-pub enum Object {
+#[derive(Clone, Debug)]
+pub enum Obj {
     Null,
     Bool(bool),
     Int(i64),
     Float(f64),
     String(String),
-    Object(HashMap<String, Object>),
-    Array(Vec<Object>),
+    Object(HashMap<String, Obj>),
+    Array(Vec<Obj>),
 }
 
 pub struct JSON {
-    pub json: Object,
+    pub json: Obj,
 }
 
 // impl Index<Object> for JSON {
@@ -26,9 +26,8 @@ pub struct JSON {
 // }
 
 impl JSON {
-    pub fn parse(string: String) -> Object {
+    pub fn parse(string: String) -> Obj {
         for line in string.lines() {
-
         }
         todo!()
     }
@@ -38,34 +37,36 @@ impl JSON {
 mod tests {
     use super::*;
 
-    fn make_json() -> Object {
+
+    fn make_json() -> Obj {
         let mut map = HashMap::new();
 
-        map.insert("null".to_string(), Object::Null);
-        map.insert("boolean".to_string(), Object::Bool(true));
-        map.insert("integer".to_string(), Object::Int(1));
-        map.insert("float".to_string(), Object::Float(std::f64::consts::PI));
-        map.insert("string".to_string(), Object::String("Hello, world!".to_string()));
+        map.insert("null".to_string(), Obj::Null);
+        map.insert("boolean".to_string(), Obj::Bool(true));
+        map.insert("integer".to_string(), Obj::Int(1));
+        map.insert("float".to_string(), Obj::Float(std::f64::consts::PI));
+        map.insert("string".to_string(), Obj::String("Hello, world!".to_string()));
 
         let nested_map = HashMap::from([
-            ("nested_object".to_string(), Object::Object(map.clone()))
+            ("nested_object".to_string(), Obj::Object(map.clone()))
         ]);
-        map.insert("map".to_string(), Object::Object(nested_map));
+        map.insert("map".to_string(), Obj::Object(nested_map));
 
         let array= map.clone().into_values().collect();
-        map.insert("array".to_string(), Object::Array(array));
-
-        Object::Object(map)
+        map.insert("array".to_string(), Obj::Array(array));
+        
+        println!("{:?}", map);
+        Obj::Object(map)
     }
     
     #[test]
     fn smoke_test() {
         let json = make_json();
         match json {
-            Object::Object(x) => {
-                print!("{:?}", x.len());
-                assert_eq!(x.len(), 7);
-            },
+            Obj::Object(json) => {
+                assert_eq!(json.len(), 7);
+                assert_eq!(Obj::Null, json);
+            } 
             _ => panic!("Test JSON should be JSON Object at the top level!"),
         }
     }
