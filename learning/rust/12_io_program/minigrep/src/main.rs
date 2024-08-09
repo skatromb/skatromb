80e82aeb1c0099846474b58015b3de31354ec7e1
skatromb <1759463+skatromb@users.noreply.github.com>
2024-08-09 14:21:13 +0200

    io program

--- learning/rust/12_io_program/minigrep/src/main.rs
+++ learning/rust/12_io_program/minigrep/src/main.rs
@@ -1,36 +1,14 @@
-use std::env;
-use std::fs;
 use std::process;
+use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build().unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-    
-    println!("With text: \n {contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments")
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Self { query, file_path })
-    }    
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
