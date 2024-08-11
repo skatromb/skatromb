65d26f2b507e94f54da78aa82fb930b077468ea2
skatromb <1759463+skatromb@users.noreply.github.com>
2024-08-11 09:39:45 +0200

    rc re-work

--- learning/rust/15_pointers/rc/src/main.rs
+++ learning/rust/15_pointers/rc/src/main.rs
@@ -1,29 +1,24 @@
 use std::rc::Rc;
+use std::cell::{RefCell};
 use crate::List::{Cons, Nil};
 
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
 fn main() {
-    let a = Rc::new(
-        Cons(5, Rc::new(
-            Cons(10, Rc::new(
-                Nil)
-            )
-        ))
-    );
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating v = {}", Rc::strong_count(&a));
+    let value = Rc::new(RefCell::new(5));
 
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("count after c goes out of scopes = {}", Rc::strong_count(&a));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
     
+    *value.borrow_mut() += 10;
     
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
 }
