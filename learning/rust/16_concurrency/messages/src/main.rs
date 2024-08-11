a240c8e19251b1a73c845ee426d667461b0133ea
skatromb <1759463+skatromb@users.noreply.github.com>
2024-08-11 21:48:58 +0200

    styling

--- learning/rust/16_concurrency/messages/src/main.rs
+++ learning/rust/16_concurrency/messages/src/main.rs
@@ -4,22 +4,22 @@ use std::{thread, time};
 fn main() {
     let (tx, rx) = mpsc::channel();
     let _tx1 = tx.clone();
-    
+
     thread::spawn(move || {
         let val = "hi".to_string();
         tx.send(val).unwrap();
     });
-    
+
     let received: String;
     let timer = time::Instant::now();
     loop {
         println!("Trying to get the message");
-        
+
         if let Ok(rec) = rx.try_recv() {
             received = rec;
-            break 
+            break
         }
     }
-    
+
     println!("Received!: {received} in {:?}", timer.elapsed());
 }
