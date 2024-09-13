901066d53eb986b7261afdd818c358be69349e48
skatromb <1759463+skatromb@users.noreply.github.com>
2024-09-13 22:55:37 +0200

    Refactored

--- learning/rust/files_reducer/src/main.rs
+++ learning/rust/files_reducer/src/main.rs
@@ -1,10 +1,11 @@
-use std::io::{Write};
-use std::thread;
 use std::fs;
 use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
+use std::thread;
 
+/// Get all file paths
 fn get_file_paths(directory: &str) -> impl Iterator<Item = PathBuf> {
     let entries = fs::read_dir(directory).unwrap();
 
@@ -13,33 +14,33 @@ fn get_file_paths(directory: &str) -> impl Iterator<Item = PathBuf> {
         .filter(|path| path.is_file())
 }
 
-fn make_mutex_file(file_name: &str) ->  Arc<Mutex<File>> {
+/// Make file to write as multithreaded version
+fn make_mutex_file(file_name: &str) -> Arc<Mutex<File>> {
     let file = fs::File::options()
-        .create(true).write(true).truncate(true)
+        .create(true)
+        .write(true)
+        .truncate(true)
         .open(file_name)
         .unwrap();
 
     Arc::new(Mutex::new(file))
 }
 
-fn reduce_in_threads(
-    file_paths: impl Iterator<Item = PathBuf>,
-    into_file_path: &str,
-) {
-    let to_file = make_mutex_file(into_file_path);
-    
-    let thread_handles = file_paths
-        .map(|path| {
-            let to_file = to_file.clone();
-            
-            thread::spawn(move || {
-                let contents = fs::read_to_string(path).unwrap();
-                
-                to_file.lock().unwrap()
-                    .write_all(contents.as_bytes()).unwrap();
-                })
-            }
-        );
+/// Read files and write to one file in threads
+fn reduce_in_threads(file_paths: impl Iterator<Item = PathBuf>, to_file: Arc<Mutex<File>>) {
+    let thread_handles = file_paths.map(|path| {
+        let to_file = to_file.clone();
+
+        thread::spawn(move || {
+            let contents = fs::read_to_string(path).unwrap();
+
+            to_file
+                .lock()
+                .unwrap()
+                .write_all(contents.as_bytes())
+                .unwrap();
+        })
+    });
 
     thread_handles
         .into_iter()
@@ -48,9 +49,10 @@ fn reduce_in_threads(
 
 fn main() {
     let paths = get_file_paths("files");
-    
-    reduce_in_threads(paths, "combined.json");
-    
+
+    let to_file = make_mutex_file("combined.json");
+
+    reduce_in_threads(paths, to_file);
 
     // Alternative simple version
     // for entry in entries {
