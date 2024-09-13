52b86819cdd9e55773edcc3b54ba71961daa71a9
skatromb <1759463+skatromb@users.noreply.github.com>
2024-09-13 22:50:27 +0200

    Fully concurrent writing to file

--- learning/rust/files_reducer/src/main.rs
+++ learning/rust/files_reducer/src/main.rs
@@ -1,7 +1,9 @@
 use std::io::{Write};
 use std::thread;
 use std::fs;
+use std::fs::File;
 use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
 
 fn get_file_paths(directory: &str) -> impl Iterator<Item = PathBuf> {
     let entries = fs::read_dir(directory).unwrap();
@@ -11,39 +13,43 @@ fn get_file_paths(directory: &str) -> impl Iterator<Item = PathBuf> {
         .filter(|path| path.is_file())
 }
 
-fn read_in_threads(file_paths: impl Iterator<Item = PathBuf>) -> impl Iterator<Item = String> {
-    let threaded_contents = file_paths
-        .map(|path|
-            thread::spawn(||
-                fs::read_to_string(path).unwrap()
-            )
-        );
+fn make_mutex_file(file_name: &str) ->  Arc<Mutex<File>> {
+    let file = fs::File::options()
+        .create(true).write(true).truncate(true)
+        .open(file_name)
+        .unwrap();
 
-    let contents = threaded_contents
-        .into_iter()
-        .map(|handler| handler.join().unwrap());
-
-    contents
+    Arc::new(Mutex::new(file))
 }
 
-fn write_file(contents: impl Iterator<Item = String>) {
-    let mut file = fs::File::options()
-        .create(true).write(true).truncate(true)
-        .open("combined.jsonl")
-        .unwrap();
+fn reduce_in_threads(
+    file_paths: impl Iterator<Item = PathBuf>,
+    into_file_path: &str,
+) {
+    let to_file = make_mutex_file(into_file_path);
     
-    contents.for_each(|content| {
-        file.write_all(content.as_bytes()).unwrap();
-    });
-    
+    let thread_handles = file_paths
+        .map(|path| {
+            let to_file = to_file.clone();
+            
+            thread::spawn(move || {
+                let contents = fs::read_to_string(path).unwrap();
+                
+                to_file.lock().unwrap()
+                    .write_all(contents.as_bytes()).unwrap();
+                })
+            }
+        );
+
+    thread_handles
+        .into_iter()
+        .for_each(|handler| handler.join().unwrap());
 }
 
 fn main() {
     let paths = get_file_paths("files");
     
-    let contents = read_in_threads(paths);
-    
-    write_file(contents);
+    reduce_in_threads(paths, "combined.json");
     
 
     // Alternative simple version
