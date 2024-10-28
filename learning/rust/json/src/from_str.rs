d694b84260840d6d42afe29b77870fa70a544937
skatromb <1759463+skatromb@users.noreply.github.com>
2024-10-28 14:00:02 +0100

    new version of `parse_string`

--- learning/rust/json/src/from_str.rs
+++ learning/rust/json/src/from_str.rs
@@ -4,25 +4,18 @@ use std::fmt::Display;
 use std::str::Chars;
 use std::str::FromStr;
 
-mod keychars {
-    pub(super) const NULL_STARTS: char = 'n';
-    pub(super) const NULL_REST: [char; 3] = ['u', 'l', 'l'];
-
-    pub(super) const STRING_STARTS: char = '"';
-    pub(super) const OBJECT_STARTS: char = '{';
-}
-
-use keychars::*;
-
 #[derive(Debug, PartialEq)]
 pub enum ParseError {
     InvalidJSON(&'static str),
     InvalidControlCharacter(&'static str),
+    InvalidEscapeCharacter(&'static str),
 }
 
 const NULL_PARSING_ERROR: ParseError = ParseError::InvalidJSON("Error while parsing `null`");
 const NEWLINE_CHARACTER_ERROR: ParseError =
     ParseError::InvalidJSON("Newline character is not allowed");
+const INVALID_ESCAPE_CHARACTER: ParseError =
+    ParseError::InvalidEscapeCharacter("Invalid escape character sequence");
 
 impl Display for ParseError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -40,18 +33,18 @@ impl FromStr for JSON {
 
 fn parse(chars: &mut Chars) -> Result<JSON, ParseError> {
     match chars.find(|ch| !ch.is_whitespace()) {
-        Some(NULL_STARTS) => {
-            parse_null(chars.by_ref())?;
+        Some('n') => {
+            parse_null(chars)?;
             Ok(JSON::Null)
         }
 
-        Some(STRING_STARTS) => {
-            let string = parse_string(chars.by_ref())?;
+        Some('"') => {
+            let string = parse_string(chars)?;
             Ok(JSON::String(string))
         }
 
-        Some(OBJECT_STARTS) => {
-            let object = parse_object(chars.by_ref())?;
+        Some('{') => {
+            let object = parse_object(chars)?;
             Ok(JSON::Object(object))
         }
 
@@ -61,6 +54,8 @@ fn parse(chars: &mut Chars) -> Result<JSON, ParseError> {
 
 /// Take 3 symbols after 'n' and expect them to be "ull"
 fn parse_null(chars: &mut Chars) -> Result<(), ParseError> {
+    const NULL_REST: [char; 3] = ['u', 'l', 'l'];
+
     if chars.take(3).eq(NULL_REST) {
         Ok(())
     } else {
@@ -70,43 +65,39 @@ fn parse_null(chars: &mut Chars) -> Result<(), ParseError> {
 
 /// Take symbols until '"' appears
 fn parse_string(chars: &mut Chars) -> Result<String, ParseError> {
-    const STOP_TAKING: bool = false;
-    const CONTINUE_TAKE: bool = true;
-
-    let mut escape_char = false;
     let mut error: Option<ParseError> = None;
+    let mut escape_char = false;
+    let mut string = String::new();
 
-    let string = chars
-        .take_while(|ch| {
-            match ch {
-                '\\' => {
-                    escape_char = !escape_char;
-                    CONTINUE_TAKE
-                }
-                'n' => {
-                    if escape_char {
-                        error = Some(NEWLINE_CHARACTER_ERROR);
-                        STOP_TAKING
-                    } else {
-                        CONTINUE_TAKE
-                    }
-                }
-                '"' => {
-                    if escape_char {
-                        escape_char = false;
-                        CONTINUE_TAKE
-                    } else {
-                        STOP_TAKING // on `"` we exit
-                    }
-                }
+    for char in chars {
+        if escape_char {
+            let escaped_char = match char {
+                '\\' => '\\',
+                '/' => '/',
+                '"' => '"',
+                'n' => '\n',
+                'r' => '\r',
+                't' => '\t',
                 _ => {
-                    escape_char = false;
-                    CONTINUE_TAKE
+                    error = Some(INVALID_ESCAPE_CHARACTER);
+                    break;
                 }
-            }
-        })
-        .collect();
+            };
 
+            string.push(escaped_char);
+            escape_char = false;
+        } else {
+            match char {
+                '\\' => escape_char = true,
+                '"' => break,
+                '\n' => {
+                    error = Some(NEWLINE_CHARACTER_ERROR);
+                    break;
+                }
+                _ => string.push(char),
+            }
+        }
+    }
     error.map_or(Ok(string), Err)
 }
 
@@ -160,25 +151,51 @@ mod tests {
 
     #[test]
     fn parse_string_happy() {
-        let mut chars = r#"something""#.chars();
-        let parsed = parse_string(chars.by_ref()).unwrap();
+        let mut chars = r#"string""#.chars();
+        let parsed = parse_string(&mut chars).unwrap();
 
-        assert_eq!(parsed, "something".to_string())
+        assert_eq!(parsed, "string")
     }
 
     #[test]
     fn parse_string_quotes() {
-        let mut chars = r#"something\" ""#.chars();
-        let parsed = parse_string(chars.by_ref()).unwrap();
+        let mut chars = r#" `\"` ""#.chars();
+        let parsed = parse_string(&mut chars).unwrap();
 
-        assert_eq!(parsed, r#"something\" "#.to_string())
+        assert_eq!(parsed, " `\"` ")
     }
 
     #[test]
-    fn parse_string_escapes() {
-        let mut chars = r#"\\something\\ ""#.chars();
-        let parsed = parse_string(chars.by_ref()).unwrap();
+    fn parse_string_escapes_happy() {
+        let mut chars = r#" `\\` ""#.chars();
+        let parsed = parse_string(&mut chars).unwrap();
 
-        assert_eq!(parsed, r#"\\something\\ "#.to_string())
+        assert_eq!(parsed, " `\\` ")
+    }
+
+    #[test]
+    fn parse_string_escapes_fail() {
+        let mut chars = r#" `\` ""#.chars();
+        let parsed = parse_string(&mut chars).err().unwrap();
+
+        assert_eq!(parsed, INVALID_ESCAPE_CHARACTER)
+    }
+
+    #[test]
+    fn parse_string_newline_happy() {
+        let mut chars = r#"newline! \n another line""#.chars();
+        let parsed = parse_string(&mut chars).unwrap();
+
+        assert_eq!(parsed, "newline! \n another line")
+    }
+
+    #[test]
+    fn parse_string_newline_fail() {
+        let mut chars = r"newline!
+            another line"
+            .chars();
+        let parsed = parse_string(&mut chars).err().unwrap();
+
+        assert_eq!(parsed, NEWLINE_CHARACTER_ERROR)
     }
 }
