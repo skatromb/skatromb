f94bc650375f72098edc0e4b0a6774b95cb82849
skatromb <1759463+skatromb@users.noreply.github.com>
2025-06-13 17:33:38 +0200

    cargo fmt

--- learning/rust/16_concurrency/threads/src/main.rs
+++ learning/rust/16_concurrency/threads/src/main.rs
@@ -1,25 +1,31 @@
 use std::thread;
-use std::time::{Instant, Duration};
+use std::time::{Duration, Instant};
 
 fn main() {
     dbg!(thread::available_parallelism().unwrap());
     let timer = Instant::now();
     let handle = thread::spawn(|| {
         for i in 1..10 {
-            println!("hi number {i} from the spawned thread {:?}!", thread::current().id());
+            println!(
+                "hi number {i} from the spawned thread {:?}!",
+                thread::current().id()
+            );
             thread::sleep(Duration::from_millis(1));
         }
     });
     println!("{}", timer.elapsed().as_nanos());
-    
+
     for i in 1..5 {
-        println!("hi number {i} from main thread {:?}!", thread::current().id());
+        println!(
+            "hi number {i} from main thread {:?}!",
+            thread::current().id()
+        );
         thread::sleep(Duration::from_millis(1));
     }
 
     while !dbg!(handle.is_finished()) {
         thread::sleep(Duration::from_millis(1));
     }
-    
-    handle.join().unwrap();
+
+    handle.join();
 }
