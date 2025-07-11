a8a6092bbe5050637c530b22ff108b7770c964e4
skatromb <1759463+skatromb@users.noreply.github.com>
2025-07-11 18:07:04 +0200

    move rust learnings

--- learning/rust/04_ownership/sandbox/src/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let a = [1, 2, 3, 4, 5];
-    let slice = &a[1..3];
-    assert!(slice == &[2, 3]);
-}
--- learning/rust/07_packages_crates_modules/sandbox/src/main.rs
+++
@@ -1,8 +0,0 @@
-use std::collections::HashMap;
-use std::fmt::Result as FmtResult;
-use std::io::Result as IoResult;
-
-fn main() {
-    let mut map = HashMap::new();
-    map.insert(1,2);
-}
